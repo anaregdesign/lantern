2b2c176dd34f74b1de90e6546cb5e430da003d4c
piroyoung <piroyoung@users.noreply.github.com>
2023-05-03 17:29:23 +0900

    init

--- server/provider/provider.go
+++ server/provider/provider.go
@@ -5,16 +5,30 @@ import (
 	"github.com/anaregdesign/papaya/cache/graph"
 	"google.golang.org/grpc"
 	"net"
+	"os"
+	"strconv"
 	"time"
 )
 
 type Config struct {
-	ttl time.Duration
+	ttl  time.Duration
+	port int
 }
 
 func NewConfig() *Config {
+	ttl, err := strconv.Atoi(os.Getenv("DEFAULT_TTL_SECONDS"))
+	if err != nil {
+		ttl = 60
+	}
+
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		port = 6380
+	}
+
 	return &Config{
-		ttl: 1 * time.Minute,
+		ttl:  time.Duration(ttl) * time.Second,
+		port: port,
 	}
 }
 
@@ -23,7 +37,7 @@ func NewGraphCache(c *Config) *graph.GraphCache[string, *v1.Vertex] {
 }
 
 func NewListener() (net.Listener, error) {
-	return net.Listen("tcp", ":8080")
+	return net.Listen("tcp", ":"+strconv.Itoa(NewConfig().port))
 }
 
 func NewGrpcServerOptions() []grpc.ServerOption {
