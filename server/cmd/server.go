936ed4003ab9209df8f22920d4f3166f0f34be82
Hiroki Mizukami <6128022+piroyoung@users.noreply.github.com>
2023-05-08 19:45:06 +0900

    add: comment (#8)
    Co-authored-by: piroyoung <piroyoung@users.noreply.github.com>

--- server/cmd/server.go
+++ server/cmd/server.go
@@ -10,6 +10,10 @@ import (
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
+	log.Println("Environment Variables:")
+	for _, pair := range os.Environ() {
+		log.Println(pair)
+	}
 
 	signalCh := make(chan os.Signal)
 	defer close(signalCh)
@@ -25,6 +29,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	log.Println("Starting Lantern Server")
 	if err := svr.Run(ctx); err != nil {
 		log.Fatal(err)
 	}
